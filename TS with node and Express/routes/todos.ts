8148858c8a7c0f76fd351a8821915cf39f32cd04
abbi964 <abhinavthapliyal964@gmail.com>
2023-05-12 13:14:05 +0530

    TS with node and Express

---
+++ TS with node and Express/routes/todos.ts
@@ -0,0 +1,45 @@
+import { Router } from "express";
+
+import { Todo } from "../models/todo";
+
+const router = Router()
+
+let todos : Todo[] = []
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({todos : todos})
+})
+
+router.post('/todo',(req,res,next)=>{
+    let newTodo : Todo = {
+        id : new Date().toISOString(),
+        text : req.body.text
+    }
+
+    todos.push(newTodo);
+    res.status(200).json('todo added')
+})
+
+router.post('/delete',(req,res,next)=>{
+    let id = req.body.id;
+    for(let i = 0 ; i < todos.length ; i++){
+        if (todos[i].id === id){
+            todos.splice(i,1)
+            res.status(200).json({msg:'deletion sucessfull'})
+        }
+    }
+    res.status(404).json('item not found')
+})
+
+router.post('/edit',(req,res,next)=>{
+    let id = req.body.id;
+    for(let i = 0 ; i < todos.length ; i++){
+        if (todos[i].id === id){
+            todos[i].text = req.body.text
+            res.status(200).json('sucessfully edited')
+        }
+    }
+    res.status(404).json('item not found')
+})
+
+export default router;
